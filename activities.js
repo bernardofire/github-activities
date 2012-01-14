17c8412afc4b76f776dc41f62e77fdb8da8ca61d
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-01-14 00:01:06 -0200

    accept others users

--- activities.js
+++ activities.js
@@ -2,18 +2,16 @@
     function parse(feed, container) {
         if (feed.entries.length > 0) {
             $(feed.entries).each(function(key, entry){
-                var author = entry.author;
-                var title = entry.title;
-                var link = entry.link;
                 var content = entry.contentSnippet;
                 //convert special chars and remove unnecessary "   ..." from content
                 var description = $('<a />').html(content).text().trim().replace(new RegExp("[\n ]+...$"),'')
-
-                var li_entry = $('<li />').text(title)
+                var li_entry = $('<li />').text(entry.title)
                 var div_entry = $('<a />').append(li_entry)
-                div_entry.attr('href', link);
-                div_entry.attr('target', '_blank');
-                div_entry.attr('title', description);
+                div_entry.attr({
+                    'href': entry.link,
+                    'target': '_blank',
+                    'title': description
+                });
                 $(container).append(div_entry);
             });
         }
@@ -26,10 +24,12 @@
                     'username':'bernardofire',
                 }
 
+                var settings = $.extend(defaults, options);
+
                 return this.each(function() {
                     var container = $(this)
                     $.ajax({
-                        url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent('http://github.com/' + defaults.username + '.atom'),
+                        url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent('http://github.com/' + settings.username + '.atom'),
                         async: true,
                         dataType: 'json',
                         success: function(data) {
