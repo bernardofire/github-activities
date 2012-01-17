daea16e963e3a9de2e46b24e78a08566a0152c8b
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-01-17 03:10:31 -0200

    add support for hide username

--- activities.js
+++ activities.js
@@ -1,12 +1,15 @@
 (function($) {
-    function parse(feed, container) {
+    function parse(feed, container, settings) {
         if (feed.entries.length > 0) {
             $(feed.entries).each(function(key, entry){
                 var content = entry.contentSnippet;
                 //convert special chars and remove unnecessary "   ..." from content
-                var description = $('<a />').html(content).text().trim().replace(new RegExp("[\n ]+...$"),'')
-                var li_entry = $('<li />').text(entry.title)
-                var div_entry = $('<a />').append(li_entry)
+                var description = $('<a />').html(content).text().trim().replace(new RegExp("[\n ]+...$"),'');
+                var title = entry.title;
+                if (settings.hide_username)
+                    title = title.replace(new RegExp('^' + entry.author + ' '), '');
+                var li_entry = $('<li />').text(title);
+                var div_entry = $('<a />').append(li_entry);
                 div_entry.attr({
                     'href': entry.link,
                     'target': '_blank',
@@ -22,6 +25,7 @@
             function(options) {
                 var defaults = {
                     'username': '',
+                    'hide_username': false,
                 }
 
                 var settings = $.extend(defaults, options);
@@ -33,7 +37,7 @@
                         async: true,
                         dataType: 'json',
                         success: function(data) {
-                            parse(data.responseData.feed, container);
+                            parse(data.responseData.feed, container, settings);
                         }
                     });
                 });
